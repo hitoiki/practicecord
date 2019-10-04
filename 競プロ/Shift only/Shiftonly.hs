4cdce82f9194a28e33b0008ec5595da685b53c73
hitoiki <hitoiki103ggmail.com>
2019-10-05 08:46:11 +0900

    Shiftonly

--- 競プロ/Shift only/Shiftonly.hs
+++ 競プロ/Shift only/Shiftonly.hs
@@ -6,7 +6,12 @@ module Main where
     main :: IO ()
     main = do
     
-
-    s <- getLine
+    ignore <- getLine --なんか使わずに済んだので無視
+    arg <- map (\x -> read(x)::Integer) . words <$> getLine
     
-    putStrLn $ show (a + b + c) ++ " " ++ s
+    putStrLn $ show . minimum $ map (factorization 2) arg
+                                where
+                                    factorization :: Integer -> Integer -> Integer
+                                    factorization a b  --bを素因数分解した時のaの係数
+                                        |mod b a == 0 = 1 + factorization a (div b a)
+                                        |otherwise = 0
